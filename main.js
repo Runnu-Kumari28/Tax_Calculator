4416f35428762132e8c1f300ffb8b510dbf9cff1
runnu_kumari <runnukumari9901@gmail.com>
2024-04-15 17:18:20 +0530

    feat: added new modal and improved validations

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
 // input field validation
-function parseNumber(input) 
-{
+function parseNumber(input){
+  const form = document.getElementById('taxCalculator');
+  form.classList.remove("was-validated"); // To remove bootstrap existing form validation
+
   const inputValue = input.value.trim();
-  const parsedValue = parseInt(inputValue);
-  
-  if (isNaN(parsedValue) || parsedValue < 0) 
-  {
+  if (isNaN(inputValue) || parseInt(inputValue) < 0){
      // Display error message using tooltip
-     var tooltip = new bootstrap.Tooltip
-     (input, 
-      {
+    const tooltip = new bootstrap.Tooltip(input, {
       title: "Please enter positive numbers only.",
       placement: "right",
       trigger: "manual"
-      }
-    );
+    });
     tooltip.show();
     input.classList.add("is-invalid");
-  } else 
-  {
+  } else {
     // Input is valid, reset error message and styling
-    var tooltip = bootstrap.Tooltip.getInstance(input);
+    const tooltip = bootstrap.Tooltip.getInstance(input);
     if (tooltip) {
       tooltip.dispose(); 
     }
@@ -30,10 +25,11 @@ function parseNumber(input)
 
 // initialise tooltip
 document.addEventListener("DOMContentLoaded", function() {
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-});});
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
+});
 
 function calculateTax(age, income, extraIncome, deductions) {
   const grossIncome = income + extraIncome;
@@ -50,7 +46,7 @@ function calculateTax(age, income, extraIncome, deductions) {
   // tax amount
   let tax = 0;
   if (grossIncome > 800000) {
-      tax = taxRate * (grossIncome - deductions - 800000);
+      tax = taxRate * (grossIncome - (deductions + 800000));
       const finalIncome = grossIncome - tax;
       return finalIncome; 
   } else {
@@ -58,32 +54,26 @@ function calculateTax(age, income, extraIncome, deductions) {
   }
 }
 
-function submitForm()
-{ 
+function submitForm(){
   const forms = document.querySelectorAll('.needs-validation');
-  let isValid = true;
-
   Array.from(forms).forEach(form => {
     if (!form.checkValidity()) {
       form.classList.add('was-validated');
-      isValid = false;
+      return
     }
   });
-
-  if (!isValid) return;
   
-  const grossAnnualIncome = parseInt(document.getElementById('gross_annual_income').value);
-  const extraIncome = parseInt(document.getElementById('extra_income').value);
-  const ageGroup = parseInt(document.getElementById('age_group').value);
-  const applicableDeductions = parseInt(document.getElementById('applicable_deductions').value);
+  const grossAnnualIncome = parseFloat(document.getElementById('gross_annual_income').value);
+  const extraIncome = parseFloat(document.getElementById('extra_income').value);
+  const ageGroup = parseFloat(document.getElementById('age_group').value);
+  const applicableDeductions = parseFloat(document.getElementById('applicable_deductions').value);
   
   const income = calculateTax(ageGroup, grossAnnualIncome, extraIncome, applicableDeductions);
 
   if (!isNaN(income)) {
     // Display tax in modal body
     const taxParagraph = document.getElementById('tax-info');
-    taxParagraph.textContent = "Final-Income after Tax deduction: Rs " + income;
-
+    taxParagraph.textContent = "After tax deductions your overall income will be : Rs " + income.toLocaleString('en-IN');
     let myModal = new bootstrap.Modal(document.getElementById('dataModel'), {});
     myModal.show();
   } 
